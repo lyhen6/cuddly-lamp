a07254bdb4c6bb3877aa73c60b3c7c102c437e70
liuyuan <liuyuan@317hu.com>
2020-04-22 17:41:59 +0800

    zk相关 实现zk序列化

---
+++ cuddly-lamp-service/src/main/java/commont/Common.java
@@ -0,0 +1,4 @@
+package commont;
+
+public class Common {
+}
---
+++ cuddly-lamp-service/src/main/java/commont/zk/MyZkSerializer.java
@@ -0,0 +1,33 @@
+package commont.zk;
+
+import org.I0Itec.zkclient.exception.ZkMarshallingError;
+import org.I0Itec.zkclient.serialize.ZkSerializer;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ *  实现序列化接口
+ */
+public class MyZkSerializer implements ZkSerializer {
+
+    @Override
+    public byte[] serialize(Object o) throws ZkMarshallingError {
+        String data = (String) o;
+        try {
+            return data.getBytes("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public Object deserialize(byte[] bytes) throws ZkMarshallingError {
+        try {
+            return new String(bytes, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
